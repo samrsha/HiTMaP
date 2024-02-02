f0fb243a541b4d0fc75b46fc47de53750c2b13fd
qihu6986 <qihu6986@uni.sydney.edu.au>
2024-02-02 12:01:26 +1100

    debugging running_snakemake.sh

--- running_snakemake.sh
+++ running_snakemake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 cd workflow
-conda activate hitmap-snakemake-test-1
+conda activate hitmap-snakemake
 
 # ------------------------------ Ask for the number of cores and validate input ------------------------------
 while true; do
@@ -17,8 +17,8 @@ done
 while true; do
     read -p "Please specify the folder name to save the output for this run (no spaces): " folder_name
     if [[ ! $folder_name =~ \ |\' ]]; then
-        mkdir -p data/output
-        if [ -d "data/output/$folder_name" ]; then
+        mkdir -p data/Output
+        if [ -d "data/Output/$folder_name" ]; then
             echo "This folder name already exists. Please choose another one."
         else
             break
@@ -28,7 +28,7 @@ while true; do
     fi
 done
 
-# Retrieve the name of the .ibd file and copy/link the corresponding ID folder
+# get the name of the .ibd file and copy/link the corresponding ID folder
 ibd_file_name=$(basename data/*.ibd .ibd)
 
 # ------------------------------ Ask which module to run and validate ------------------------------
@@ -41,28 +41,25 @@ while true; do
     
     case $module_choice in
         1)
+            echo "Snakemake pipeline starts now..."
             snakemake --cores "$cores" data/Summary\ folder/protein_index.csv
-            echo "reference data baseing"
-            break
             ;;
         2)
+            echo "Snakemake pipeline starts now..."
             snakemake --cores "$cores" data/$ibd_file_name\ ID/Peptide_region_file.csv
-            echo "peak picking ing"
-            break
             ;;
         3)
+            echo "Snakemake pipeline starts now..."
             snakemake --cores "$cores" data/Summary\ folder/Protein_feature_list_trimmed.csv
-            echo "image rendering ing"
-            break
             ;;
         *)
             echo "Invalid option. Please enter 1, 2, or 3."
+            continue
             ;;
     esac
+    break
 done
 
 # ------------------------------ Copy or link results to the output folder ------------------------------
-(cp -rl data/Summary\ folder "data/output/$folder_name" || cp -r data/Summary\ folder "data/output/$folder_name") 2> /dev/null
-(cp -rl "data/${ibd_file_name} ID" "data/output/$folder_name" || cp -r "data/${ibd_file_name} ID" "data/output/$folder_name") 2> /dev/null
-
-echo "Process completed. Results are saved in data/output/$folder_name"
+(cp -rl data/Summary\ folder "data/Output/$folder_name" || cp -r data/Summary\ folder "data/Output/$folder_name") 2> /dev/null
+(cp -rl "data/${ibd_file_name} ID" "data/Output/$folder_name" || cp -r "data/${ibd_file_name} ID" "data/Output/$folder_name") 2> /dev/null
